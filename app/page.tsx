3c9bfb5f019e3b13a8b61e222726a465d13d9f4d
Safo Emmanuel <nanasafo122@gmail.com>
2023-07-29 06:25:36 +0000

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,39 @@
+
+import getListings from './actions/getListings';
+import ClientOnly from './components/ClientOnly'
+import Container from './components/Container'
+import EmptyState from './components/EmptyState';
+import ListingCard from './components/listings/ListingCard';
+
+
+export default async function Home() {
+  const listings = await getListings();
+  const isEmpty = true;
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset/>
+      </ClientOnly>
+    )
+  }
+  
+  return (
+   <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3
+        lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8
+        ">
+          <div>{listings.map((listing: any) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                data={listing}
+              />
+            )
+          })}</div>
+        </div>
+      </Container>
+   </ClientOnly>
+  )
+}
